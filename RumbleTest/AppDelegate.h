afb1bde45f9665777b3e7edd65e684d0c70ac862
王景伟 <496116842@qq.com>
2020-11-05 10:43:49 +0800

    init

---
+++ RumbleTest/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  RumbleTest
+//
+//  Created by 王景伟 on 2020/11/5.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
